6b6a8aca2bca0ca9671d14806868ace8f1c49fe7
Gonzalo Nardini <nardinigonzalom@gmail.com>
2021-05-29 19:22:37 -0300

    db config

--- packages/notification-service/knexfile.ts
+++ packages/notification-service/knexfile.ts
@@ -1,46 +1,19 @@
+const connection = {
+  client: 'postgresql',
+  connection: {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    database: process.env.DB_DATABASE,
+    user: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+  },
+  migrations: {
+    tableName: 'knex_migrations',
+  },
+}
+
 module.exports = {
-  development: {
-    // client: "sqlite3",
-    // connection: {
-    //   filename: "./dev.sqlite3"
-    // }
-    client: 'postgresql',
-    connection: {
-      database: 'indexer_mainnet',
-      user: 'postgres',
-      password: 'docker',
-    },
-  },
-
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'indexer_mainnet',
-      user: 'postgres',
-      password: 'docker',
-    },
-    pool: {
-      min: 2,
-      max: 10,
-    },
-    migrations: {
-      tableName: 'knex_migrations',
-    },
-  },
-
-  production: {
-    client: 'postgresql',
-    connection: {
-      database: 'indexer_mainnet',
-      user: 'postgres',
-      password: 'docker',
-    },
-    pool: {
-      min: 2,
-      max: 10,
-    },
-    migrations: {
-      tableName: 'knex_migrations',
-    },
-  },
+  development: connection,
+  staging: connection,
+  production: connection,
 }
--- packages/notification-service/src/indexer/db.ts
+++ packages/notification-service/src/indexer/db.ts
@@ -3,10 +3,11 @@ import knex from 'knex'
 const knexInstance = knex({
   client: 'pg',
   connection: {
-    host: '127.0.0.1',
-    user: 'postgres',
-    password: 'docker',
-    database: 'indexer_mainnet',
+    host: process.env.DB_HOST ?? 'localhost',
+    port: parseInt(process.env.DB_PORT ?? '5432', 10),
+    database: process.env.DB_DATABASE ?? 'indexer',
+    user: process.env.DB_USERNAME ?? 'postgres',
+    password: process.env.DB_PASSWORD ?? 'docker',
   },
 })
 
